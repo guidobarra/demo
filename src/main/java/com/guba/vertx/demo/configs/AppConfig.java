a6ae4025fdd13757cfbd4843616f11b2302489c7
guidobarra <guidobarra18@gmail.com>
2025-05-03 15:42:07 -0300

    fix config

--- src/main/java/com/guba/vertx/demo/configs/AppConfig.java
+++ src/main/java/com/guba/vertx/demo/configs/AppConfig.java
@@ -7,28 +7,32 @@ import java.io.InputStream;
 import java.util.Map;
 
 public class AppConfig {
-  private static JsonObject config;
 
-  public static void load(String resourcePath) {
-    Yaml yaml = new Yaml();
-    InputStream input = AppConfig.class.getClassLoader().getResourceAsStream(resourcePath);
+    private static JsonObject config;
 
-    if (input == null) {
-      throw new RuntimeException("Archivo de configuración no encontrado: " + resourcePath);
+    private AppConfig() {
     }
 
-    Map<String, Object> map = yaml.load(input);
-    config = new JsonObject(map);
-  }
+    public static void load(String resourcePath) {
+        Yaml yaml = new Yaml();
+        InputStream input = AppConfig.class.getClassLoader().getResourceAsStream(resourcePath);
+
+        if (input == null) {
+            throw new IllegalArgumentException("Archivo de configuración no encontrado: " + resourcePath);
+        }
+
+        Map<String, Object> map = yaml.load(input);
+        config = new JsonObject(map);
+    }
 
     public static JsonObject get() {
-    if (config == null) {
-      throw new IllegalStateException("AppConfig no ha sido cargado. Llamá primero a load()");
+        if (config == null) {
+            throw new IllegalStateException("AppConfig no ha sido cargado. Llamá primero a load()");
+        }
+        return config;
     }
-    return config;
-  }
 
-  public static JsonObject section(String key) {
-    return get().getJsonObject(key);
-  }
+    public static JsonObject section(String key) {
+        return get().getJsonObject(key);
+    }
 }
